2e924dcc767de973df6f3f70b64f8fe5ddf89e16
School pc <you@example.com>
2021-10-21 14:42:35 +0200

    Exercise 7 finished

--- ex7/taskFA.c
+++ ex7/taskFA.c
@@ -12,6 +12,8 @@ bool eat1=0, eat2=0, eat3=0, eat4=0, eat5=0;
 
 void *philosopher1(void* args)
     {   
+        while (1)
+      {
         printf("P1 requires fork 1\n");
         pthread_mutex_lock(&mtx1);
         fork1 =  1;
@@ -28,11 +30,15 @@ void *philosopher1(void* args)
         fork2 = 0;
         pthread_mutex_unlock(&mtx2);
         printf("P1 releases fork 2\n");
+      }
+      
         return NULL;
     }
 
 void *philosopher2(void* args)
     {
+        while (1)
+        {
         printf("P2 requires fork 2\n");
         pthread_mutex_lock(&mtx2);
         fork2 =  1;
@@ -49,12 +55,16 @@ void *philosopher2(void* args)
         fork3 = 0;
         pthread_mutex_unlock(&mtx3);
         printf("P2 releases fork 3\n");
+        }
+        
         return NULL;
     }
 
 void *philosopher3(void* args)
     {
-       printf("P3 requires fork 3\n");
+       while (1)
+       {
+           printf("P3 requires fork 3\n");
         pthread_mutex_lock(&mtx3);
         fork3 =  1;
         printf("P3 has fork 3\n");
@@ -70,49 +80,59 @@ void *philosopher3(void* args)
         fork4 = 0;
         pthread_mutex_unlock(&mtx4);
         printf("P3 releases fork 4\n");
+       }
+       
         return NULL;
     }
 
 void *philosopher4(void* args)
     {
-       printf("P4 requires fork 4\n");
-        pthread_mutex_lock(&mtx4);
-        fork4 =  1;
-        printf("P4 has fork 4\n");
-        printf("P4 requires fork 5\n");
-        pthread_mutex_lock(&mtx5);
-        printf("P4 has fork 5\n");
-        fork5 = 1;
-        printf("Philosopher 4 is now eating!\n");
-        eat4=1;
-        fork4 = 0;
-        pthread_mutex_unlock(&mtx4);
-        printf("P4 releases fork 4\n");
-        fork5 = 0;
-        pthread_mutex_unlock(&mtx5);
-        printf("P4 releases fork 5\n");
+       while (1)
+       {
+           printf("P4 requires fork 4\n");
+            pthread_mutex_lock(&mtx4);
+            fork4 =  1;
+            printf("P4 has fork 4\n");
+            printf("P4 requires fork 5\n");
+            pthread_mutex_lock(&mtx5);
+            printf("P4 has fork 5\n");
+            fork5 = 1;
+            printf("Philosopher 4 is now eating!\n");
+            eat4=1;
+            fork4 = 0;
+            pthread_mutex_unlock(&mtx4);
+            printf("P4 releases fork 4\n");
+            fork5 = 0;
+            pthread_mutex_unlock(&mtx5);
+            printf("P4 releases fork 5\n");
+       }
+       
         return NULL;
     }
 
 void *philosopher5(void* args)
     {
-       printf("P5 requires fork 5\n");
-        pthread_mutex_lock(&mtx5);
-        fork5 =  1;
-        printf("P5 has fork 5\n");
-        printf("P5 requires fork 1\n");
-        pthread_mutex_lock(&mtx1);
-        printf("P5 has fork 1\n");
-        fork1 = 1;
-        printf("Philosopher 5 is now eating!\n");
-        eat5=1;
-        fork5 = 0;
-        pthread_mutex_unlock(&mtx5);
-        printf("P5 releases fork 5\n");
-        fork1 = 0;
-        pthread_mutex_unlock(&mtx1);
-        printf("P5 releases fork 1\n");
-        return NULL;
+       while (1)
+       {
+            printf("P5 requires fork 5\n");
+            pthread_mutex_lock(&mtx5);
+            fork5 =  1;
+            printf("P5 has fork 5\n");
+            printf("P5 requires fork 1\n");
+            pthread_mutex_lock(&mtx1);
+            printf("P5 has fork 1\n");
+            fork1 = 1;
+            printf("Philosopher 5 is now eating!\n");
+            eat5=1;
+            fork5 = 0;
+            pthread_mutex_unlock(&mtx5);
+            printf("P5 releases fork 5\n");
+            fork1 = 0;
+            pthread_mutex_unlock(&mtx1);
+            printf("P5 releases fork 1\n");
+       }
+       return NULL;
+       
     }
       
 int main()
@@ -126,28 +146,35 @@ int main()
         pthread_mutex_init(&mtx4,NULL);
         pthread_mutex_init(&mtx5,NULL);
         
+        pthread_create(&thread1, NULL, philosopher1, NULL);
+        pthread_create(&thread2, NULL, philosopher2, NULL);
+        pthread_create(&thread3, NULL, philosopher3, NULL);
+        pthread_create(&thread4, NULL, philosopher4, NULL);
+        pthread_create(&thread5, NULL, philosopher5, NULL);
+
+        pthread_join(thread1, NULL);
+        pthread_join(thread2, NULL);
+        pthread_join(thread3, NULL);
+        pthread_join(thread4, NULL);
+        pthread_join(thread5, NULL);
+        
         
 
-        /*bool wait=false;
+        bool wait=false;
         while( !(eat1 && eat2 && eat3 && eat4 && eat5) ){
             wait = true;
-        }*/
+        }
 
-        bool wait=false;
+       /* bool wait=false;
         while (1)
         {
-           pthread_create(&thread1, NULL, philosopher1, NULL);
-            pthread_create(&thread2, NULL, philosopher2, NULL);
-            pthread_create(&thread3, NULL, philosopher3, NULL);
-            pthread_create(&thread4, NULL, philosopher4, NULL);
-            pthread_create(&thread5, NULL, philosopher5, NULL);
-
             pthread_join(thread1, NULL);
             pthread_join(thread2, NULL);
             pthread_join(thread3, NULL);
             pthread_join(thread4, NULL);
             pthread_join(thread5, NULL);
-        }
+           
+        }*/
         
         pthread_mutex_destroy(&mtx1);
         pthread_mutex_destroy(&mtx2);
--- ex7/taskFB.c
+++ ex7/taskFB.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdbool.h>
+#include <unistd.h>
 // Note the argument and return types: void*
 
 long global_variable = 0;
@@ -12,106 +13,183 @@ bool eat1=0, eat2=0, eat3=0, eat4=0, eat5=0;
 
 void *philosopher1(void* args)
     {   
-        printf("P1 requires fork 1\n");
-        pthread_mutex_lock(&mtx1);
-        fork1 =  1;
-        printf("P1 has fork 1\n");
-        printf("P1 requires fork 2\n");
-        pthread_mutex_lock(&mtx2);
-        printf("P1 has fork 2\n");
-        fork2 = 1;
-        printf("Philosopher 1 is now eating!\n");
-        eat1=1;
-        fork1 = 0;
-        pthread_mutex_unlock(&mtx1);
-        printf("P1 releases fork 1\n");
-        fork2 = 0;
-        pthread_mutex_unlock(&mtx2);
-        printf("P1 releases fork 2\n");
+        int mtx1_locked, mtx2_locked;
+
+        while (1)
+        {
+            mtx1_locked=1;
+            mtx2_locked=1;
+            while (mtx1_locked!=0 || mtx2_locked!=0)
+            {
+                mtx1_locked=1;
+                mtx2_locked=1;
+                usleep(1);
+                mtx1_locked = pthread_mutex_trylock(&mtx1);
+                if (mtx1_locked==0)
+                {
+                    mtx2_locked = pthread_mutex_trylock(&mtx2);
+                }
+                
+                if (mtx2_locked!=0 && mtx1_locked==0)
+                {
+                    pthread_mutex_unlock(&mtx1);
+                }
+            }
+            printf("Philosopher 1 has locked F1 and F2\n");
+            eat1=1;
+            printf("Philosopher 1 is now eating!\n");
+            pthread_mutex_unlock(&mtx1);
+            pthread_mutex_unlock(&mtx2);
+            printf("Philosopher 1 releases fork 1 and 2\n");
+        }
+        
+        
         return NULL;
     }
 
 void *philosopher2(void* args)
     {
-        printf("P2 requires fork 2\n");
-        pthread_mutex_lock(&mtx2);
-        fork2 =  1;
-        printf("P2 has fork 2\n");
-        printf("P2 requires fork 3\n");
-        pthread_mutex_lock(&mtx3);
-        printf("P2 has fork 3\n");
-        fork3 = 1;
-        printf("Philosopher 2 is now eating!\n");
-        eat2=1;
-        fork2 = 0;
-        pthread_mutex_unlock(&mtx2);
-        printf("P2 releases fork 2\n");
-        fork3 = 0;
-        pthread_mutex_unlock(&mtx3);
-        printf("P2 releases fork 3\n");
+        int mtx2_locked, mtx3_locked;
+
+        while (1)
+        {
+            mtx2_locked=1;
+            mtx3_locked=1;
+            while (mtx2_locked!=0 || mtx3_locked!=0)
+            {
+                mtx2_locked=1;
+                mtx3_locked=1;
+                usleep(1);
+                mtx2_locked = pthread_mutex_trylock(&mtx2);
+                if (mtx2_locked==0)
+                {
+                    mtx3_locked = pthread_mutex_trylock(&mtx3);
+                }
+                
+                if (mtx3_locked!=0 && mtx2_locked==0)
+                {
+                    pthread_mutex_unlock(&mtx2);
+                }
+            }
+            printf("Philosopher 2 has locked F2 and F3\n");
+            eat2=1;
+            printf("Philosopher 2 is now eating!\n");
+            pthread_mutex_unlock(&mtx2);
+            pthread_mutex_unlock(&mtx3);
+            printf("Philosopher 2 releases fork 2 and 3\n");
+        }
+        
+        
         return NULL;
     }
 
 void *philosopher3(void* args)
     {
-       printf("P3 requires fork 3\n");
-        pthread_mutex_lock(&mtx3);
-        fork3 =  1;
-        printf("P3 has fork 3\n");
-        printf("P3 requires fork 4\n");
-        pthread_mutex_lock(&mtx4);
-        printf("P3 has fork 4\n");
-        fork4 = 1;
-        printf("Philosopher 3 is now eating!\n");
-        eat3=1;
-        fork3 = 0;
-        pthread_mutex_unlock(&mtx3);
-        printf("P3 releases fork 3\n");
-        fork4 = 0;
-        pthread_mutex_unlock(&mtx4);
-        printf("P3 releases fork 4\n");
+        int mtx3_locked, mtx4_locked;
+
+        while (1)
+        {
+            mtx3_locked=1;
+            mtx4_locked=1;
+            while (mtx3_locked!=0 || mtx4_locked!=0)
+            {
+                mtx3_locked=1;
+                mtx4_locked=1;
+                usleep(1);
+                mtx3_locked = pthread_mutex_trylock(&mtx3);
+                if (mtx3_locked==0)
+                {
+                    mtx4_locked = pthread_mutex_trylock(&mtx4);
+                }
+                
+                if (mtx4_locked!=0 && mtx3_locked==0)
+                {
+                    pthread_mutex_unlock(&mtx3);
+                }
+            }
+            printf("Philosopher 3 has locked F3 and F4\n");
+            eat3=1;
+            printf("Philosopher 3 is now eating!\n");
+            pthread_mutex_unlock(&mtx3);
+            pthread_mutex_unlock(&mtx4);
+            printf("Philosopher 3 releases fork 3 and 4\n");
+        }
+        
+        
+        
         return NULL;
     }
 
 void *philosopher4(void* args)
     {
-       printf("P4 requires fork 4\n");
-        pthread_mutex_lock(&mtx4);
-        fork4 =  1;
-        printf("P4 has fork 4\n");
-        printf("P4 requires fork 5\n");
-        pthread_mutex_lock(&mtx5);
-        printf("P4 has fork 5\n");
-        fork5 = 1;
-        printf("Philosopher 4 is now eating!\n");
-        eat4=1;
-        fork4 = 0;
-        pthread_mutex_unlock(&mtx4);
-        printf("P4 releases fork 4\n");
-        fork5 = 0;
-        pthread_mutex_unlock(&mtx5);
-        printf("P4 releases fork 5\n");
+       int mtx4_locked, mtx5_locked;
+
+        while (1)
+        {
+            mtx4_locked=1;
+            mtx5_locked=1;
+            while (mtx4_locked!=0 || mtx5_locked!=0)
+            {
+                mtx4_locked=1;
+                mtx5_locked=1;
+                usleep(1);
+                mtx4_locked = pthread_mutex_trylock(&mtx4);
+                if (mtx4_locked==0)
+                {
+                    mtx5_locked = pthread_mutex_trylock(&mtx5);
+                }
+                
+                if (mtx5_locked!=0 && mtx4_locked==0)
+                {
+                    pthread_mutex_unlock(&mtx4);
+                }
+            }
+            printf("Philosopher 4 has locked F4 and F5\n");
+            eat4=1;
+            printf("Philosopher 4 is now eating!\n");
+            pthread_mutex_unlock(&mtx4);
+            pthread_mutex_unlock(&mtx5);
+            printf("Philosopher 4 releases fork 4 and 5\n");
+        }
+        
+        
         return NULL;
     }
 
 void *philosopher5(void* args)
     {
-       printf("P5 requires fork 5\n");
-        pthread_mutex_lock(&mtx5);
-        fork5 =  1;
-        printf("P5 has fork 5\n");
-        printf("P5 requires fork 1\n");
-        pthread_mutex_lock(&mtx1);
-        printf("P5 has fork 1\n");
-        fork1 = 1;
-        printf("Philosopher 5 is now eating!\n");
-        eat5=1;
-        fork5 = 0;
-        pthread_mutex_unlock(&mtx5);
-        printf("P5 releases fork 5\n");
-        fork1 = 0;
-        pthread_mutex_unlock(&mtx1);
-        printf("P5 releases fork 1\n");
+       int mtx5_locked, mtx1_locked;
+
+        while (1)
+        {
+            mtx5_locked=1;
+            mtx1_locked=1;
+            while (mtx5_locked!=0 || mtx1_locked!=0)
+            {
+                mtx5_locked=1;
+                mtx1_locked=1;
+                usleep(1);
+                mtx5_locked = pthread_mutex_trylock(&mtx5);
+                if (mtx5_locked==0)
+                {
+                    mtx1_locked = pthread_mutex_trylock(&mtx1);
+                }
+                
+                if (mtx1_locked!=0 && mtx5_locked==0)
+                {
+                    pthread_mutex_unlock(&mtx5);
+                }
+            }
+            printf("Philosopher 5 has locked F5 and F1\n");
+            eat5=1;
+            printf("Philosopher 5 is now eating!\n");
+            pthread_mutex_unlock(&mtx5);
+            pthread_mutex_unlock(&mtx1);
+            printf("Philosopher 5 releases fork 5 and 1\n");
+        }
+        
+        
+        
         return NULL;
     }
       
@@ -125,30 +203,28 @@ int main()
         pthread_mutex_init(&mtx3,NULL);
         pthread_mutex_init(&mtx4,NULL);
         pthread_mutex_init(&mtx5,NULL);
-        
-        
+    
 
-        /*bool wait=false;
-        while( !(eat1 && eat2 && eat3 && eat4 && eat5) ){
-            wait = true;
-        }*/
+         
+        pthread_create(&thread1, NULL, philosopher1, NULL);
+        pthread_create(&thread2, NULL, philosopher2, NULL);
+        pthread_create(&thread3, NULL, philosopher3, NULL);
+        pthread_create(&thread4, NULL, philosopher4, NULL);
+        pthread_create(&thread5, NULL, philosopher5, NULL);
+
+        pthread_join(thread1, NULL);
+        pthread_join(thread2, NULL);
+        pthread_join(thread3, NULL);
+        pthread_join(thread4, NULL);
+        pthread_join(thread5, NULL);
+        
+        
 
         bool wait=false;
-        while (1)
-        {
-           pthread_create(&thread1, NULL, philosopher1, NULL);
-            pthread_create(&thread2, NULL, philosopher2, NULL);
-            pthread_create(&thread3, NULL, philosopher3, NULL);
-            pthread_create(&thread4, NULL, philosopher4, NULL);
-            pthread_create(&thread5, NULL, philosopher5, NULL);
-
-            pthread_join(thread1, NULL);
-            pthread_join(thread2, NULL);
-            pthread_join(thread3, NULL);
-            pthread_join(thread4, NULL);
-            pthread_join(thread5, NULL);
+        while( !(eat1 && eat2 && eat3 && eat4 && eat5) ){
+            wait = true;
         }
-        
+
         pthread_mutex_destroy(&mtx1);
         pthread_mutex_destroy(&mtx2);
         pthread_mutex_destroy(&mtx3);
